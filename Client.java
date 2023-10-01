50c01d8dd418bc42fed62f9ec13fb6725a07a7ab
Angryplayer321 <agayevayxan581@gmail.com>
2023-10-01 17:32:00 +0400

    Errors Handled

--- Client.java
+++ Client.java
@@ -12,64 +12,71 @@ public class Client {
     // initialize socket and input output streams
     private Socket socket = null;
     private DataInputStream input = null;
-    private DataOutputStream out = null;
+
 
     // constructor to put ip address and port
     public Client(String address, int[] portNumbers) {
-// establish a connection
+        // establish a connection
         String line = "";
-        while (true) {
+        int serverNumber;
+        Socket[] socket = new Socket[portNumbers.length];
+        DataOutputStream[] out = new DataOutputStream[portNumbers.length];
+
+        for (int i = 0; i < portNumbers.length; i++) {
             try {
-                Random random = new Random();
-                int serverNumber = random.nextInt(portNumbers.length);
-                socket = new Socket(address, portNumbers[serverNumber]);
-                System.out.println("Connected to Server " + serverNumber + "on port : " + portNumbers[serverNumber]);
-// takes input from terminal
-                input = new DataInputStream(System.in);
-// sends output to the socket
-                out = new DataOutputStream(socket.getOutputStream());
+                socket[i] = new Socket(address, portNumbers[i]);
+                out[i] = new DataOutputStream(socket[i].getOutputStream());
             } catch (UnknownHostException u) {
                 System.out.println(u);
-            } catch (IOException i) {
+            } catch (IOException io) {
                 System.out.println(i);
-            }// string to read message from input
-
-// keep reading until "Over" is input
-
+            }
+        }
+        while (true) {
+            Random random = new Random();
+            serverNumber = random.nextInt(portNumbers.length);
+            System.out.println("Connected to Server " + serverNumber + " on port : " + portNumbers[serverNumber]);
+            // takes input from terminal
+            input = new DataInputStream(System.in);
+            // sends output to the socket
             try {
 
                 line = input.readLine();
-                out.writeUTF(line);
                 if (line.equals("Over")) {
 
                     break;
                 }
+                out[serverNumber].writeUTF(line);
+
             } catch (IOException i) {
                 System.out.println(i);
             }
         }
-// close the connection
-try
-{
-input.close();
-out.close();
-socket.close();
-}
-catch(IOException i)
-{
-System.out.println(i);
-}
-}
-public static void main(String args[]) {
-    System.out.println("Please enter 1st, 2nd and 3rd servers' port numbers in according sequence!");
-    Scanner sc = new Scanner(System.in);
-    int[] portNumbers = new int[3];
-    for (int i = 0; i < portNumbers.length; i++) {
-        portNumbers[i] = sc.nextInt();
+        // close the connection
+        try {
+            input.close();
+            for (int i = 0; i < portNumbers.length; i++) {
+                out[i].writeUTF("Over");
+                out[i].close();
+                socket[i].close();
+
+            }
+
+        } catch (IOException i) {
+            System.out.println(i);
+        }
     }
 
-    Client client = new Client("127.0.0.1", portNumbers);
+    public static void main(String[] args) {
+        System.out.println("Please enter 1st, 2nd and 3rd servers' port numbers in according sequence!");
+        Scanner sc = new Scanner(System.in);
+        int[] portNumbers = new int[3];
+        for (int i = 0; i < portNumbers.length; i++) {
+            portNumbers[i] = sc.nextInt();
+        }
+
+        Client client = new Client("127.0.0.1", portNumbers);
 
 
-}
+    }
 }
--- Server.java
+++ Server.java
@@ -5,49 +5,51 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.SQLOutput;
 import java.util.Scanner;
-public class Server
-{
-//initialize socket and input stream
-private Socket socket = null;
-private ServerSocket server = null;
-private DataInputStream in = null;
-// constructor with port
-public Server(int port)
-{
+
+public class Server {
+    //initialize socket and input stream
+    private Socket socket = null;
+    private ServerSocket server = null;
+    private DataInputStream in = null;
+
+    // constructor with port
+    public Server(int port) {
 // starts server and waits for a connection
-try{
-server = new ServerSocket(port);
-System.out.println("Server started");
-System.out.println("Waiting for a client ...");
-socket = server.accept();
-System.out.println("Client accepted");
+        try {
+            server = new ServerSocket(port);
+            System.out.println("Server started");
+            System.out.println("Waiting for a client ...");
+            socket = server.accept();
+            System.out.println("Client accepted");
 // takes input from the client socket
-in = new DataInputStream(
-new BufferedInputStream(socket.getInputStream()));
-String line = "";
+            in = new DataInputStream(
+                    new BufferedInputStream(socket.getInputStream()));
+            String line = "";
 // reads message from client until "Over" is sent
-while (!line.equals("Over"))
-{
-try {
-    line = in.readUTF();
-    System.out.println(Integer.parseInt(line) * 2);
+            while (!line.equals("Over")) {
+                try {
+                    line = in.readUTF();
+                    System.out.println(Integer.parseInt(line) * 2);
 
 
-}
-catch(IOException i)
-{
-System.out.println(i);
-}
-}
-    System.out.println("Closing connection");
+                } catch (IOException i) {
+                    System.out.println(i);
+                } catch (NumberFormatException n)
+                {
+                    System.out.println(n);
+                    System.out.println("Please use proper number format without whitespaces!");
+                }
+            }
+            System.out.println("Closing connection");
 // close connection
-    socket.close();
-    in.close();
-} catch (IOException i) {
-    System.out.println(i);
-}
-}
+            socket.close();
+            in.close();
+        } catch (IOException i) {
+            System.out.println(i);
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("Please enter the server port!");
